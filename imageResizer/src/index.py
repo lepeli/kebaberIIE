e186871fe57d2731a094a1a9d09a144b21d10eea
lepeli <accounts@yvan.dev>
2024-11-23 19:52:30 +0100

    We can finally get jobs from redis git add imageResizer/

--- imageResizer/src/index.py
+++ imageResizer/src/index.py
@@ -4,14 +4,21 @@ import time
 
 # Connect to redis server
 
+r = None
 
-# try:
-#     r = Redis(host="redis", port=6379, decode_responses=True)
-# except:
-#     print("error")
+try:
+    r = Redis(host="redis", port=6379, decode_responses=True)
+    print("The redis device has been connected successfuly")
+except:
+    print("error")
 
 
 while True:
-
-    print("en attente de job...")
-    time.sleep(15)
+    try:
+        job = r.brpop("compressing_queue", timeout=60)
+        if job == None:
+            print("Didn't have a job, going to France emploi (asking redis for a new job)")
+        else:
+            print(f"Received job: {job}")
+    except Exception as e:
+        print(f"Got error: {e}")
