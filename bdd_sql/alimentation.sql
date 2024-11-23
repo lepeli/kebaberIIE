c59f8de4cb6d4950eef3562b585b84828e4589af
lepeli <accounts@yvan.dev>
2024-11-23 15:00:49 +0100

    Added way to auto initialize the database

--- bdd_sql/alimentation.sql
+++ bdd_sql/alimentation.sql
@@ -1,9 +1,9 @@
 
 INSERT INTO Restaurant (nom, adresse, code_postal, site_web, url_photo)
 VALUES
-    ('Klassiker',' 12 Cr Mgr Roméro, 91000 Évry-Courcouronnes', 
+    ('Klassiker',' 12 Cr Mgr Roméro, 91000 Évry-Courcouronnes',
     '91000', 'https://theklassiker.fr/','https://b3386818.smushcdn.com/3386818/wp-content/uploads/2021/11/The-Klassiker-Logo-e1636226243760.png?lossy=1&strip=1&webp=1'),
-    ('Restaurant Anatolia', '14 Cr Mgr Roméro, 91000 Évry-Courcouronnes', 
+    ('Restaurant Anatolia', '14 Cr Mgr Roméro, 91000 Évry-Courcouronnes',
     '91000', NULL, 'https://lh3.googleusercontent.com/p/AF1QipObPLGRXtUaslQIwDI2VSs2EsOU8MeSqgktUpY5=s680-w680-h510')
     ;
 
