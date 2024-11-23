4e2ded7eea586e1c9c79f9d5f747969fcfb2e9ce
Adeline Berthelot <adeline.berthelot@ensiie.eu>
2024-11-23 16:07:31 +0100

    ajout de l'id et du prix pour les données des restaurants

--- bdd_sql/alimentation.sql
+++ bdd_sql/alimentation.sql
@@ -1,15 +1,15 @@
 
-INSERT INTO Restaurant (nom, adresse, code_postal, site_web, url_photo)
+INSERT INTO Restaurant (id, nom, adresse, code_postal, site_web, url_photo, prix)
 VALUES
-    ('Klassiker',' 12 Cr Mgr Roméro, 91000 Évry-Courcouronnes',
-    '91000', 'https://theklassiker.fr/','https://b3386818.smushcdn.com/3386818/wp-content/uploads/2021/11/The-Klassiker-Logo-e1636226243760.png?lossy=1&strip=1&webp=1'),
+    (1,'Klassiker',' 12 Cr Mgr Roméro, 91000 Évry-Courcouronnes',
+    '91000', 'https://theklassiker.fr/','https://b3386818.smushcdn.com/3386818/wp-content/uploads/2021/11/The-Klassiker-Logo-e1636226243760.png?lossy=1&strip=1&webp=1', 12.90),
     ('Restaurant Anatolia', '14 Cr Mgr Roméro, 91000 Évry-Courcouronnes',
-    '91000', NULL, 'https://lh3.googleusercontent.com/p/AF1QipObPLGRXtUaslQIwDI2VSs2EsOU8MeSqgktUpY5=s680-w680-h510')
+    '91000', NULL, 'https://lh3.googleusercontent.com/p/AF1QipObPLGRXtUaslQIwDI2VSs2EsOU8MeSqgktUpY5=s680-w680-h510',9.00)
     ;
 
 INSERT INTO Avis (restaurant, commentaire, note, url_photo)
 VALUES
-    ('Klassiker', 'Très bon restaurant. Service agréable et rapide', 9,
+    (2,'Klassiker', 'Très bon restaurant. Service agréable et rapide', 9,
     NULL),
     ('Klassiker', 'Très bon rapport qualité prix. Service rapide si on commande par internet', 8,
     NULL),
